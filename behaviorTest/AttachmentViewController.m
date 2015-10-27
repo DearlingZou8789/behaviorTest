b250db01e98bfed97deba34d5efe1b97b7545847
zmj27404 <zmj27404@sina.cn>
2015-10-27 22:45:01 +0800

    增加了吸附行为的动画效果，但存在问题就行KVO无法监听到boxView中的frame属性，网上说是位置其实没有改变，而是图层，明天看看这个网址http://bbs.csdn.net/topics/390722123“

--- behaviorTest/AttachmentViewController.m
+++ behaviorTest/AttachmentViewController.m
@@ -8,8 +8,19 @@
 
 #import "AttachmentViewController.h"
 
-@interface AttachmentViewController ()
-
+@interface AttachmentViewController ()<UICollisionBehaviorDelegate>
+{
+    UIDynamicAnimator *animator;
+    UIGravityBehavior *gravity;
+    UICollisionBehavior *collision;
+    UIAttachmentBehavior *attachment;
+    UIDynamicItemBehavior *dynamicBehavior;
+    
+    UIView *boxView;
+    UIView *pointView;
+    UIView *barrierView;
+    BOOL isGravity;
+}
 @end
 
 @implementation AttachmentViewController
@@ -17,7 +28,107 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.navigationController.navigationBar.translucent = YES;
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    boxView = [[UIView alloc] initWithFrame:CGRectMake(100, 0, 70, 70)];
+    boxView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:boxView];
+    [boxView addObserver:self forKeyPath:@"frame" options:0 context:NULL];
+
+    boxView.userInteractionEnabled = YES;
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panView:)];
+    [boxView addGestureRecognizer:pan];
+    
+    barrierView = [[UIView alloc] initWithFrame:CGRectMake(0, 300, 120, 2)];
+    barrierView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:barrierView];
+    
+    pointView = [[UIView alloc] initWithFrame:CGRectMake(120, 250, 10, 10)];
+    pointView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:pointView];
+    pointView.layer.cornerRadius = pointView.bounds.size.width / 2.0;
+    pointView.layer.borderColor = pointView.backgroundColor.CGColor;
+    pointView.layer.borderWidth = 1.0f;
+
+    animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    //重力行为
+    gravity = [[UIGravityBehavior alloc] initWithItems:@[boxView]];
+    gravity.gravityDirection = CGVectorMake(0.0, 0.1);
+    //碰撞行为
+    collision = [[UICollisionBehavior alloc] initWithItems:@[boxView]];
+    [collision addBoundaryWithIdentifier:@"barrier" fromPoint:barrierView.frame.origin toPoint:CGPointMake(barrierView.frame.origin.x + barrierView.frame.size.width, barrierView.frame.origin.y)];
+    collision.translatesReferenceBoundsIntoBoundary = YES;
+    collision.collisionDelegate = self;
+    //行为限制
+    dynamicBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[boxView]];
+    dynamicBehavior.elasticity = 0.5;
 }
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"frame"])
+    {
+        CGRect rect = [change[@"new"] CGRectValue];
+        NSLog(@"rect = %@", NSStringFromCGRect(rect));
+        if (rect.origin.y + rect.size.height == self.view.frame.size.height)
+        {
+            isGravity = NO;
+            [animator removeAllBehaviors];
+        }
+    }
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animator];
+    [self addBehavior];
+}
+
+- (void)panView:(UIPanGestureRecognizer *)pan
+{
+    if (isGravity)
+    {
+        return;
+    }
+    CGRect rect = pan.view.frame;
+    CGPoint point = [pan translationInView:self.view];
+    pan.view.frame = CGRectOffset(rect, point.x, point.y);
+    [pan setTranslation:CGPointZero inView:self.view];
+}
+
+- (void)addBehavior
+{
+    [animator addBehavior:gravity];
+    [animator addBehavior:collision];
+    [animator addBehavior:dynamicBehavior];
+}
+
+- (void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier atPoint:(CGPoint)p
+{
+    NSString *identStr = (NSString *)identifier;
+    isGravity = YES;
+    if ([identStr isEqualToString:@"barrier"])
+    {
+        attachment = [[UIAttachmentBehavior alloc] initWithItem:pointView attachedToItem:boxView];
+        [animator addBehavior:attachment];
+    }
+}
+
+- (void)collisionBehavior:(UICollisionBehavior *)behavior endedContactForItem:(id<UIDynamicItem>)item1 withItem:(id<UIDynamicItem>)item2
+{
+    
+}
+
+
+- (void)dealloc
+{
+    NSLog(@"attachmentViewController did dealloc");
+    [boxView removeObserver:self forKeyPath:@"frame"];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];

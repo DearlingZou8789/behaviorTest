c4e08f9b7527c4fe2f70a1f07ec64c18f0622800
zmj27404 <zmj27404@sina.cn>
2015-10-28 22:43:29 +0800

    添加了平移仿射变换，让蓝色视图动起来了,这是一种隐式动画效果

--- behaviorTest/CoreAnimationViewController.m
+++ behaviorTest/CoreAnimationViewController.m
@@ -11,6 +11,9 @@
 @interface CoreAnimationViewController ()
 {
     CALayer *ballLayer;
+    UIView *planView;
+    
+    CGRect souceRect, toRect;
 }
 @end
 
@@ -31,6 +34,39 @@
     ballLayer.borderWidth = 1.0f;
     ballLayer.borderColor = [UIColor greenColor].CGColor;
     [self.view.layer addSublayer:ballLayer];
+    
+    souceRect = CGRectMake(140, 20, 30, 30);
+    toRect = CGRectMake(300, 600, 30, 30);
+    planView = [[UIView alloc] initWithFrame:souceRect];
+    planView.backgroundColor = [UIColor blueColor];
+    planView.layer.opacity = 0.25;
+    [self.view addSubview:planView];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapOnce:)];
+    planView.userInteractionEnabled = YES;
+    [planView addGestureRecognizer:tap];
+    
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(120, 20, 30, 30) cornerRadius:5];
+    CAShapeLayer *shapLayer = [CAShapeLayer layer];
+    shapLayer.frame = planView.frame;
+    shapLayer.path = path.CGPath;
+    shapLayer.fillColor = [UIColor greenColor].CGColor;
+    [self.view.layer addSublayer:shapLayer];
+}
+
+- (void)tapOnce:(UITapGestureRecognizer *)tap
+{
+    CGAffineTransform moveTransform;
+    if (CGRectEqualToRect(tap.view.frame, souceRect))
+    {
+        moveTransform = CGAffineTransformMakeTranslation(CGRectGetMinX(toRect) - CGRectGetMinX(souceRect), CGRectGetMinY(toRect) - CGRectGetMinY(souceRect));
+    }
+    else if(CGRectEqualToRect(tap.view.frame, toRect))
+    {
+        moveTransform = CGAffineTransformMakeTranslation(CGRectGetMinX(souceRect) - CGRectGetMinX(toRect), CGRectGetMinY(souceRect) - CGRectGetMinY(toRect));
+    }
+    [planView.layer setAffineTransform:moveTransform];
+    planView.layer.opacity = 1.0f;
 }
 
 - (void)didReceiveMemoryWarning {
